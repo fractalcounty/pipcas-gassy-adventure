f27420bb3348734687266afdf5ef317a8aed077c
fractalcounty <121070110+fractalcounty@users.noreply.github.com>
2023-02-28 23:53:36 -0600

    Parallax refactor

---
+++ scenes/background/infinite_scrolling_background.gd
@@ -0,0 +1,9 @@
+extends PreviewingParallaxBackground
+
+@export var speed = 100.0
+
+@export var direction = Vector2.DOWN
+
+func _process(delta):
+
+	scroll_base_offset += (speed * direction) * delta
